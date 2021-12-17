1e7edbe189ac3d4a881cabf442fd43bcb6a394b1
Christian Verdin <37609068+ChristianVerdin@users.noreply.github.com>
2021-12-17 01:33:48 -0600

    Update TikTok-Salary-App.py

--- TikTok-Salary-App.py
+++ TikTok-Salary-App.py
@@ -2,7 +2,6 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 import pandas as pd
-import plotly.express as px
 import matplotlib.pyplot as plt
 import seaborn as sns
 from collections import Counter
